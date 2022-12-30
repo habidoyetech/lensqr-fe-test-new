6e74743e7353d2c81a039dc4d47e4b26e2463dcf
Habidoye97 <abidoyeabiodun97@gmail.com>
2022-12-30 19:27:48 +0100

    Create the layout for the header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import LogoWord from '../Images/lendsqr.svg'
+import LogoSign from '../Images/Union.svg'
 
 const Header: React.FC = () => {
   return (
@@ -6,7 +8,24 @@ const Header: React.FC = () => {
       <header>
         <div>
           <div>
-            
+            <img src={LogoSign} alt="lendsqr-logo-sign" />
+            <img src={LogoWord} alt="lendsqr-logo-name" />
+          </div>
+          <div>
+            <div>
+              <input type="text" placeholder='Search for anything'/>
+            </div>
+            <div>
+              <span>Search</span>
+            </div>
+          </div>
+          <div>
+            <span>Docs</span>
+            <div>Bell</div>
+            <div>
+              <img src="" alt="" />
+              <h3>Adedeji</h3>
+            </div>
           </div>
         </div>
 
