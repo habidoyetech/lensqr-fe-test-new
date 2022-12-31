36630a8e9ce26900e2011584c907fc4d7ce7adab
Habidoye97 <abidoyeabiodun97@gmail.com>
2022-12-31 10:49:17 +0100

    Add classname to the element and begin styling

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
-import LogoWord from '../Images/lendsqr.svg'
-import LogoSign from '../Images/Union.svg'
+import LogoWord from '../Images/lendsqr.svg';
+import LogoSign from '../Images/Union.svg';
+import './header.scss'
 
 const Header: React.FC = () => {
   return (
     <>
-      <header>
-        <div>
-          <div>
-            <img src={LogoSign} alt="lendsqr-logo-sign" />
-            <img src={LogoWord} alt="lendsqr-logo-name" />
+      <header className='header'>
+        <div className='header-container'>
+          <div className='lendsqr-logo-container'>
+            <img className='lensqr-logo-sign' src={LogoSign} alt="lendsqr-logo-sign" />
+            <img className='lendsqr-logo-sign' src={LogoWord} alt="lendsqr-logo-name" />
+          </div>
+          <div className='search-container'>
+            <div className='search-input-container'>
+              <input className='search-input' type="text" placeholder='Search for anything'/>
+            </div>
+            <div className='search-icon-container'>
+              <span className='search-icon'>Search</span>
+            </div>
           </div>
           <div>
-            <div>
-              <input type="text" placeholder='Search for anything'/>
-            </div>
-            <div>
-              <span>Search</span>
-            </div>
-          </div>
-          <div>
-            <span>Docs</span>
-            <div>Bell</div>
-            <div>
-              <img src="" alt="" />
-              <h3>Adedeji</h3>
+            <span className='docs'>Docs</span>
+            <div className='bells'>Bell</div>
+            <div className='user-container'>
+              <img className='user-photo' src="" alt="" />
+              <h3 className='username'>Adedeji</h3>
             </div>
           </div>
         </div>
